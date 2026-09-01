d03cf11d5e10ce664f05e83bc4941939304f541a
24f3004979 <madhavprapanna@gmail.com>
2026-09-01 10:13:18 +0530

    Mool information tree initiated with working node traversal functions with tests

--- server/src/server/service/mool.py
+++ server/src/server/service/mool.py
@@ -5,6 +5,10 @@ Root Information repository instance
     + building root node tree
 '''
 from networkx import DiGraph
+import networkx as nx
+
+from server.exceptions import DependencyClash
+from server.dev_log import log
 
 class ConceptGraph(DiGraph):
     '''
@@ -18,3 +22,34 @@ class ConceptGraph(DiGraph):
         '''
         self.add_node(topic)  # Topic added
         return self
+
+    def add_dependency(self, prerequisite:str, topic:str) -> 'ConceptGraph':
+        '''
+        Topic dependency addition
+        Fall back Error with circle dependency problem
+        '''
+        self.add_edge(prerequisite, topic)
+
+        if not nx.is_directed_acyclic_graph(self):
+            log.warning("Action Terminated for making cycle condition at root node")
+            self.remove_edge(prerequisite, topic)  # Reverting changes
+            raise DependencyClash(f'Addition of {prerequisite}->{topic} connection is making cycle')
+        # Returning Object after check about dependency
+        return self
+
+    def add_dependencies(self, prerequisite_list:list[str], topic:str) -> 'ConceptGraph':
+        for concept in prerequisite_list:
+            self.add_dependency(concept, topic)
+        return self
+
+    def downgrade_concept(self, topic:str) -> list[str]:
+        if self.in_degree(topic) == 0:
+            log.info(f'Target Node :{topic} is super parent root')
+            return [] # Root Node query
+        return list(self.predecessors(topic))
+
+    def upgrade_concept(self, topic:str) -> list[str]:
+        if self.out_degree(topic) == 0:
+            log.info(f"Target Node : {topic} is leaf node with no branches")
+            return []
+        return list(self.successors(topic))
--- server/tests/test_mool.py
+++ server/tests/test_mool.py
@@ -1,12 +1,73 @@
 from server.service import ConceptGraph
 import matplotlib.pyplot as plt
 import networkx as nx
+import pytest
 
-def test_mool():
-    g = ConceptGraph()
-    r = g.add_concept('Algebra')
+from server.exceptions import *
+from server.dev_log import log
 
-    print(g)
+# Global Single object for testing
+g = ConceptGraph()
+
+topics = [
+        "Arithmatic",
+        "Variables",
+        "Expression",
+        "Order of Operation",
+        "Simplification"
+        ]
+
+def visualize(): 
     nx.draw(g, with_labels=True)
     plt.show()
-    assert type(r) == type(r)
+
+
+
+def test_mool_root_addition():
+    '''
+    Testing Concept addition node
+    '''
+    for _ in topics:
+        g.add_concept(_)  # new topic
+
+    assert len(g) == 5 # length value should match expection
+
+
+# Cyclic dependency check testing
+def test_cycle_dependency():
+    '''
+    Testing cyclic dependency fall back and response behaviour
+    '''
+    g.add_dependency('Arithmatic', 'Order of Operation')
+    visualize()
+
+    with pytest.raises(DependencyClash):
+        g.add_dependency('Order of Operation', 'Arithmatic')
+
+def test_load_dependency():
+    dependency = [
+            'Order of Operation',
+            'Variables',
+            'Arithmatic'
+            ]
+    g.add_dependencies(dependency, 'Simplification')
+    visualize()
+
+def test_upgrade():
+    topic = g.upgrade_concept('Order of Operation')
+    log.info(f"Topic upgrade listing : {topic}")
+    assert topic[0] == 'Simplification'
+
+def test_downgrade():
+    topic = g.downgrade_concept('Simplification')
+    log.info(f'testing : {topic} <- response from downgradig concept')
+    assert topic == ['Order of Operation', 'Variables', 'Arithmatic']
+
+# Downgrade exception load
+def test_exc_upgrading():
+    t = g.upgrade_concept('Simplification')
+    assert t == []
+
+def test_exc_downgrade():
+    t = g.downgrade_concept('Arithmatic')
+    assert t == []
