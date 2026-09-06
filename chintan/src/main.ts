a6b5ea7901c7b13c8862862f5122d51b75700fbc
24f3004979 <madhavprapanna@gmail.com>
2026-09-06 22:33:59 +0530

    local update

---
+++ chintan/src/main.ts
@@ -0,0 +1,8 @@
+
+let message: string = "TS IS GREAT";
+message = 'New assignement';
+console.log(message.toLowerCase());
+
+// Need to study type cript fundamentals for approaching to solve for making chintan ;)
+
+
