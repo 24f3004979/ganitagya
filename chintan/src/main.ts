be5993ac820b0a21cd0ab05e35e666cca43b28b9
24f3004979 <madhavprapanna@gmail.com>
2026-09-15 13:29:47 +0530

    chintan foundations

--- chintan/src/main.ts
+++ chintan/src/main.ts
@@ -1,8 +1,1 @@
-
-let message: string = "TS IS GREAT";
-message = 'New assignement';
-console.log(message.toLowerCase());
-
-// Need to study type cript fundamentals for approaching to solve for making chintan ;)
-
-
+console.log('hello world');
