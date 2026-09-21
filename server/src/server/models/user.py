51c60eb3c7ffcf288ede2bd77941d69e576228c1
24f3004979 <madhavprapanna@gmail.com>
2026-09-21 22:12:14 +0530

    Data Base foundational setup

--- server/src/server/models/user.py
+++ server/src/server/models/user.py
@@ -4,34 +4,14 @@ from sqlmodel import SQLModel, Field
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy import Column
 
-"""
-Responsibility
-+ User Information Manging unit 
-+ Passoword
-+ Name
-+ Role
+# Making user model working through data base
 
-Implementation flow 
-1. Implement simple user model
-    
-    Make user model work end to end from data base to front end
-    User{
-        id : Incremental Number
-        name: Name
-        Passowrd: hashed
-        email : future mailing services requirements 
-        role : student | Admin
-    }
-
-    Student(User){
-        knowledge_graph = Meta information through the root graph of the system
-            Student Mool graph Object -> Inherited version of the root knowledge graph
-            with attached feat of mastry level of total 1st and 2nd - Based on siddhi module
-
-            Student knowledge graph providing information about student current topics and ranks topics to test out
-            Siddhi tests those listed and adjustes based on its internal mechanics with one topic hook
-                Makes final report and talks to graph for new update
-    }
-"""
-
+class User(SQLModel):
+    '''
+    Simple user model object
+    functions
+        - Uses verification function for login functionality with password
+        - Editing endpoint about user details
+        - Simple update wrappers
+    '''
 
